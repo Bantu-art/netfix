194f3650af4e5d5a8fa9f118ef3a7f742075af4f
bbantu <brianbaraza40@gmail.com.com>
2024-11-19 21:37:21 +0300

    Add views for customer and company registration and user profile
    - Created :
      - Handles customer registration using the .
      - Sets the context to distinguish customer registrations.
      - On successful form submission, redirects to the login page and shows a success message.
    
    - Created :
      - Handles company registration using the .
      - Sets the context to distinguish company registrations.
      - On successful form submission, redirects to the login page and shows a success message.
    
    - Created :
      - A detail view to display the user's profile.
      - Requires the user to be logged in ().
      - Displays the profile information of the logged-in user.
    
    These views ensure a smooth user experience for both customer and company registrations, with dedicated views for registration and profile management.

--- netfix/users/views.py
+++ netfix/users/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, DetailView
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.shortcuts import redirect
+from .forms import CustomerRegistrationForm, CompanyRegistrationForm
+from .models import User
 
-# Create your views here.
+class CustomerRegistrationView(CreateView):
+    model = User
+    form_class = CustomerRegistrationForm
+    template_name = 'users/register_customer.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_type'] = 'customer'
+        return context
+
+    def form_valid(self, form):
+        user = form.save()
+        messages.success(self.request, 'Registration successful! Please login.')
+        return redirect(self.success_url)
+
+class CompanyRegistrationView(CreateView):
+    model = User
+    form_class = CompanyRegistrationForm
+    template_name = 'users/register_company.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_type'] = 'company'
+        return context
+
+    def form_valid(self, form):
+        user = form.save()
+        messages.success(self.request, 'Registration successful! Please login.')
+        return redirect(self.success_url)
+
+class ProfileView(LoginRequiredMixin, DetailView):
+    model = User
+    template_name = 'users/profile.html'
+    context_object_name = 'profile_user'
+
+    def get_object(self):
+        return self.request.user
