65cb105cae242cca8f0b371831cc52e7d9b54348
bbantu <brianbaraza40@gmail.com.com>
2024-12-10 14:27:24 +0300

    implemented direct login on registration

--- netfix/users/views.py
+++ netfix/users/views.py
@@ -5,12 +5,13 @@ from django.contrib import messages
 from django.shortcuts import redirect
 from .forms import CustomerRegistrationForm, CompanyRegistrationForm
 from .models import User
+from django.contrib.auth import login
 
 class CustomerRegistrationView(CreateView):
     model = User
     form_class = CustomerRegistrationForm
     template_name = 'users/register_customer.html'
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('profile')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -19,14 +20,20 @@ class CustomerRegistrationView(CreateView):
 
     def form_valid(self, form):
         user = form.save()
-        messages.success(self.request, 'Registration successful! Please login.')
+        user.is_company = False
+        user.save()
+        
+        # Automatically log in the user
+        login(self.request, user)
+        
+        messages.success(self.request, 'Registration successful!')
         return redirect(self.success_url)
 
 class CompanyRegistrationView(CreateView):
     model = User
     form_class = CompanyRegistrationForm
     template_name = 'users/register_company.html'
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('profile')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -35,7 +42,13 @@ class CompanyRegistrationView(CreateView):
 
     def form_valid(self, form):
         user = form.save()
-        messages.success(self.request, 'Registration successful! Please login.')
+        user.is_company = True
+        user.save()
+        
+        # Automatically log in the user
+        login(self.request, user)
+        
+        messages.success(self.request, 'Registration successful!')
         return redirect(self.success_url)
 
 class ProfileView(LoginRequiredMixin, DetailView):
